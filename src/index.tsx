01a61366541c673a8678c08d9e8104c85d944651
Hyowook Park <hyowook_park@tmax.co.kr>
2023-01-03 18:01:47 +0900

    [feat] GNB import 주석

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Editor } from "./components/Editor";
 import "./style.css";
 import { setupLanguage } from "./java/setup";
 import { languageID } from "./java/config";
-import { GNB } from "./components/GNB/GNB";
+// import { GNB } from "./components/GNB/GNB";
 import { LNB } from "./components/LNB/LNB";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BaseAlert } from "./components/Alert/Alert";
