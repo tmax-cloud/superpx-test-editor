85cfacba06f267360a1db0e2e28811f20ba5e3dc
Hyowook Park <hyowook_park@tmax.co.kr>
2023-01-03 16:16:03 +0900

    [feat] GNB 분리

---
+++ src/components/GNB/GNB.tsx
@@ -0,0 +1,77 @@
+
+
+import * as React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+export const GNB = () => {
+    const menus = [
+      "file",
+      "edit",
+      "select",
+      "view",
+      "goto",
+      "debug",
+      "terminal",
+      "help",
+    ];
+    const menusAction = {
+      file: ["fileAction1", "fileAction2"],
+      edit: ["editAction1", "editAction2"],
+      select: ["selectAction1", "selectAction2"],
+      view: ["viewAction1", "viewAction2"],
+      goto: ["gotoAction1", "gotoAction2"],
+      debug: ["debugAction1", "debugAction2"],
+      terminal: ["terminalAction1", "terminalAction2"],
+      help: ["helpAction1", "helpAction2"],
+    };
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [openMenu, setOpenMenu] = React.useState({});
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setOpenMenu({ [event.currentTarget.value]: true });
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setOpenMenu({});
+      setAnchorEl(null);
+    };
+    return (
+      <>
+        <Link to="/">
+          <span>Home</span>
+        </Link>
+        {menus.map((menu) => {
+          return (
+            <>
+              <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                value={menu}
+              >
+                {menu}
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={openMenu[menu]}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                {menusAction[menu].map((action) => {
+                  return <MenuItem onClick={handleClose}>{action}</MenuItem>;
+                })}
+              </Menu>
+            </>
+          );
+        })}
+      </>
+    );
+  };
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,9 @@ import { Editor } from "./components/Editor";
 import "./style.css";
 import { setupLanguage } from "./java/setup";
 import { languageID } from "./java/config";
+import { GNB } from "./components/GNB/GNB";
 import { LNB } from "./components/LNB/LNB";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import { BaseAlert } from "./components/Alert/Alert";
 
 // import { language } from "./java";
@@ -51,7 +48,7 @@ const Main = () => {
   const [sourceCodeList, setSourceCodeList] = React.useState([]);
 
   return (
-    <>
+    <div>
       <LNB
         wsUrl={wsUrl}
         setEditorText={setEditorText}
@@ -64,7 +61,7 @@ const Main = () => {
       <div className="editor-area">
         <div className="title">Java Editor</div>
         <Editor
-          language={"java"}
+          language={languageID}
           wsUrl={wsUrl}
           selectedReference={selectedReference}
           fileText={editorText}
@@ -73,79 +70,10 @@ const Main = () => {
           sourceCodeList={sourceCodeList}
         />
       </div>
-    </>
+    </div>
   );
 };
 
-const GNB = () => {
-  const menus = [
-    "file",
-    "edit",
-    "select",
-    "view",
-    "goto",
-    "debug",
-    "terminal",
-    "help",
-  ];
-  const menusAction = {
-    file: ["fileAction1", "fileAction2"],
-    edit: ["editAction1", "editAction2"],
-    select: ["selectAction1", "selectAction2"],
-    view: ["viewAction1", "viewAction2"],
-    goto: ["gotoAction1", "gotoAction2"],
-    debug: ["debugAction1", "debugAction2"],
-    terminal: ["terminalAction1", "terminalAction2"],
-    help: ["helpAction1", "helpAction2"],
-  };
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [openMenu, setOpenMenu] = React.useState({});
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setOpenMenu({ [event.currentTarget.value]: true });
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setOpenMenu({});
-    setAnchorEl(null);
-  };
-  return (
-    <>
-      <Link to="/">
-        <span>Home</span>
-      </Link>
-      {menus.map((menu) => {
-        return (
-          <>
-            <Button
-              id="basic-button"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-              value={menu}
-            >
-              {menu}
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={openMenu[menu]}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              {menusAction[menu].map((action) => {
-                return <MenuItem onClick={handleClose}>{action}</MenuItem>;
-              })}
-            </Menu>
-          </>
-        );
-      })}
-    </>
-  );
-};
 const NotFound = () => {
   return <div>404 Error</div>;
 };
