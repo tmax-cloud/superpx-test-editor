e457165bf5fbdd2b0d92961224519f11fa0d9f5b
Hyowook Park <hyowook_park@tmax.co.kr>
2023-01-18 15:29:49 +0900

    [feat] Edit webpac.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devServer: {
     allowedHosts: "all",
+    historyApiFallback: true,
   },
   entry: {
     app: "./src/index.tsx",
     "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
-    "javaWorker": './src/java/java.worker.ts'
-
+    javaWorker: "./src/java/java.worker.ts",
   },
   output: {
-    globalObject: 'self',
+    globalObject: "self",
     filename: (chunkData) => {
-        switch (chunkData.chunk.name) {
-            case 'editor.worker':
-                return 'editor.worker.js';
-            case 'javaWorker':
-                return "javaWorker.js"
-            default:
-                return 'bundle.[hash].js';
-        }
+      switch (chunkData.chunk.name) {
+        case "editor.worker":
+          return "editor.worker.js";
+        case "javaWorker":
+          return "javaWorker.js";
+        default:
+          return "bundle.[hash].js";
+      }
     },
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-
     fallback: {
-      assert: require.resolve('assert'),
-      buffer: require.resolve('buffer'),
-      console: require.resolve('console-browserify'),
-      constants: require.resolve('constants-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      domain: require.resolve('domain-browser'),
-      events: require.resolve('events'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      os: require.resolve('os-browserify/browser')
+      assert: require.resolve("assert"),
+      buffer: require.resolve("buffer"),
+      console: require.resolve("console-browserify"),
+      constants: require.resolve("constants-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      domain: require.resolve("domain-browser"),
+      events: require.resolve("events"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
     },
 
     extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
-
   },
   module: {
     rules: [
@@ -54,7 +52,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
-      }
+      },
     ],
   },
   plugins: [
