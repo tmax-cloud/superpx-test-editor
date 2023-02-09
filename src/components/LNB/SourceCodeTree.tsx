6f68fc47d48c1b7685a9df6619b85dfcf1657a7d
hyeongmin-park1 <lg3319@gmail.com>
2023-02-09 17:05:45 +0900

    [patch] 변경 필요한 코드 수정

--- src/components/LNB/SourceCodeTree.tsx
+++ src/components/LNB/SourceCodeTree.tsx
@@ -1,84 +1,69 @@
 import * as React from "react";
-import { useObserver } from "mobx-react";
 import FolderTree from "react-folder-tree";
-import 'react-folder-tree/dist/style.css';
+import "react-folder-tree/dist/style.css";
 import WorkspaceStore from "../../stores/workspaceStore";
 import EditorContentsStore from "../../stores/editorContentsStore";
 
 export const SourceCodeTree = () => {
+  const onSourceCodeLinkClick = ({ nodeData }) => {
+    const { name, isOpen, content } = nodeData;
+    if (!isOpen) {
+      EditorContentsStore.updateContentAction(name, content);
+    }
+  };
 
-    const onSourceCodeLinkClick = ({nodeData}) => {
-      const {path, name, checked, isOpen, content} = nodeData
-      if(!(typeof(nodeData.isOpen)=="boolean")){
-        EditorContentsStore.updateContentAction(
-          nodeData.name,
-          nodeData.content
-        )
-      }
+  const pathToJson = (sourceCodeList) => {
+    const resultJson = {
+      name: WorkspaceStore.reference.name,
+      children: [],
+      content: [],
     };
 
-    const pathToJson = (sourceCodeList)=>{
-      const resultJson = {
-        name: (WorkspaceStore.reference.name),
-        children: [],
-        content: []
-      };
-      console.log(sourceCodeList);
-      sourceCodeList.forEach((srcList) => {
-        let node = resultJson;
-        const nodePaths = srcList.srcPath.split('/');
-        const nodeData = srcList.content;
-        const nodePath = nodePaths.shift();
+    console.log(sourceCodeList);
+    sourceCodeList.forEach((srcList) => {
+      let node = resultJson;
+      const nodePaths = srcList.srcPath.split("/");
+      const nodeData = srcList.content;
+      const nodePath = nodePaths.shift();
       while (nodePaths.length > 0) {
-        const nodePath = nodePaths.shift(); //while문 무한루프 가능성 상정
-        if (typeof node.children == "undefined"){
+        const nodePath = nodePaths.shift(); 
+        if (!node.children) {
           node.children = [];
           node.children.push({
             name: nodePath,
             children: [],
-          }  
-          );
-        }
-        else {
-        if (!node.children.map(srcList => srcList.name).includes(nodePath)) {
-          if(nodePaths.length == 0){
-            node.children.push({
-              name: nodePath,
-              content: nodeData,
-            }  
-            );
+          });
+        } else {
+          if (
+            !node.children.map((srcList) => srcList.name).includes(nodePath)
+          ) {
+            if (nodePaths.length === 0) {
+              node.children.push({
+                name: nodePath,
+                content: nodeData,
+              });
+            } else {
+              node.children.push({
+                name: nodePath,
+                children: [],
+              });
+            }
           }
-          else{
-            node.children.push({
-              name: nodePath,
-              children: [],
-            }  
-            );
-          };
-        }};
-        node = node.children.filter(pathList => pathList.name === nodePath)[0];
+        }
+        node = node.children.filter(
+          (pathList) => pathList.name === nodePath
+        )[0];
       }
-      });
-      return resultJson;
-    };
+    });
+    return resultJson;
+  };
 
-    /*const onClickAdd = () => {
-      testStore.addAction('src/component/BasicTree.tsx');
-    };
-  
-    const onClickDelete = () => {
-      testStore.deleteAction('src/component/BasicTree.tsx');
-    };*/
-
-    return (
-      <div>
-      <FolderTree
-        data={ pathToJson(WorkspaceStore.sourceCodeList) }
-        showCheckbox={ false }
-        indentPixels={ 5 }
-        onNameClick={onSourceCodeLinkClick}
-      />
-      
-      </div>
-      );
-    };
+  return (
+    <FolderTree
+      data={pathToJson(WorkspaceStore.sourceCodeList)}
+      showCheckbox={false}
+      indentPixels={5}
+      onNameClick={onSourceCodeLinkClick}
+    />
+  );
+};
