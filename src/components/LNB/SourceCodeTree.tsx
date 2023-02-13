40201a92f71f2bcda786c8234d9affdfa002cae4
hyeongmin-park1 <lg3319@gmail.com>
2023-02-13 17:17:48 +0900

    [patch] pathToJson함수 수정 및 content 분리

--- src/components/LNB/SourceCodeTree.tsx
+++ src/components/LNB/SourceCodeTree.tsx
@@ -6,53 +6,50 @@ import EditorContentsStore from "../../stores/editorContentsStore";
 
 export const SourceCodeTree = () => {
   const onSourceCodeLinkClick = ({ nodeData }) => {
-    const { name, isOpen, content } = nodeData;
+    const { name, isOpen } = nodeData;
+    const content = contentStore(name);
     if (!isOpen) {
       EditorContentsStore.updateContentAction(name, content);
     }
+  };
+  const contentStore = (name) => {
+    let content='';
+    WorkspaceStore.sourceCodeList.forEach((src)=>{
+      const srcPathArray = src.srcPath.split("/");
+      if(srcPathArray.includes(name))
+      {
+        content = src.content;
+      }
+    });
+    return content;
   };
 
   const pathToJson = (sourceCodeList) => {
     const resultJson = {
       name: WorkspaceStore.reference.name,
       children: [],
-      content: [],
     };
 
     sourceCodeList.forEach((src) => {
       let node = resultJson;
-      const nodeArray = src.srcPath.split("/");
-      const nodeData = src.content;
-      nodeArray.shift();
-      while (nodeArray.length > 0) {
-        const nodePath = nodeArray.shift();
+      const pathArray = src.srcPath.split("/");
+      let nodeArray = pathArray.filter((nodePath, index) => {
+        return index > 0;
+      });
+      nodeArray.forEach((nodePath, index, nodeArray) => {
         if (!node.children) {
           node.children = [];
+        }
+        let nameArray = node.children.map((src) => src.name);
+        if (!nameArray.includes(nodePath)) {
           node.children.push({
             name: nodePath,
-            children: [],
           });
-        } else {
-          if (
-            !node.children.map((src) => src.name).includes(nodePath)
-          ) {
-            if (nodeArray.length === 0) {
-              node.children.push({
-                name: nodePath,
-                content: nodeData,
-              });
-            } else {
-              node.children.push({
-                name: nodePath,
-                children: [],
-              });
-            }
-          }
         }
         node = node.children.filter(
           (pathList) => pathList.name === nodePath
         )[0];
-      }
+      });
     });
     return resultJson;
   };
