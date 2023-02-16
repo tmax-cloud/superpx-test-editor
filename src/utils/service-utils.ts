a8ce043289722c84d63d1257b1739aa0c6405d92
Hyowook Park <hyowook_park@tmax.co.kr>
2023-02-16 21:49:02 +0900

    [refact] setRequest 제거

--- src/utils/service-utils.ts
+++ src/utils/service-utils.ts
@@ -4,21 +4,6 @@ import { wsUrl, servicePrefix } from '../utils/constants';
 
 type ServiceCategory = 'project' | 'reference' | 'commit' | 'merge';
 
-export const setRequest = (
-  category: ServiceCategory,
-  service: string,
-  body: object,
-) => {
-  return {
-    header: {
-      targetServiceName: `${servicePrefix}.${category}.${service}`,
-      messageType: 'REQUEST',
-      contentType: 'TEXT',
-    },
-    body,
-  };
-};
-
 const createRequest = (
   category: ServiceCategory,
   service: string,
