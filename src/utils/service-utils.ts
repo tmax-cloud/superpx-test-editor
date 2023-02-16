4748f04fb7b2753861fac2ddf101fa6ec1821a01
Hyowook Park <hyowook_park@tmax.co.kr>
2023-02-16 18:53:47 +0900

    [feat] services 추가

--- src/utils/service-utils.ts
+++ src/utils/service-utils.ts
@@ -20,6 +20,21 @@ export const setRequest = (
   };
 };
 
+const createRequest = (
+  category: ServiceCategory,
+  service: string,
+  body: object,
+) => {
+  return {
+    header: {
+      targetServiceName: `${servicePrefix}.${category}.${service}`,
+      messageType: 'REQUEST',
+      contentType: 'TEXT',
+    },
+    body,
+  };
+};
+
 export const sendMessage = (
   category: ServiceCategory,
   service: string,
@@ -27,23 +42,89 @@ export const sendMessage = (
 ) => {
   WorkspaceStore.superPxWs.onopen = function (event) {
     WorkspaceStore.superPxWs.send(
-      JSON.stringify(setRequest(category, service, body)),
+      JSON.stringify(createRequest(category, service, body)),
     );
   };
 };
 
-const projectListService = () => {
+const projectInsertService = (data) => {
+  setAlert(
+    'Project Insert Service Call',
+    `Project Insert Service Call`,
+    'success',
+  );
+};
+const projectListService = (data) => {
   setAlert('Project List Service Call', `Project List Service Call`, 'success');
 };
-const projectDetailService = () => {
+const projectDetailService = (data) => {
   setAlert(
     'Project Detail Service Call',
     `Project Detail Service Call`,
     'success',
   );
 };
+const projectDeleteService = (data) => {
+  setAlert(
+    'Project Delete Service Call',
+    `Project Delete Service Call`,
+    'success',
+  );
+};
+
+const referenceInsertService = (data) => {
+  setAlert(
+    'Reference Insert Service Call',
+    `Reference Insert Service Call`,
+    'success',
+  );
+};
+const referenceListService = (data) => {
+  setAlert(
+    'Reference List Service Call',
+    `Reference List Service Call`,
+    'success',
+  );
+};
+const referenceDetailService = (data) => {
+  setAlert(
+    'Reference Detail Service Call',
+    `Reference Detail Service Call`,
+    'success',
+  );
+};
+
+const commitInsertService = (data) => {
+  setAlert(
+    'Commit Insert Service Call',
+    `Commit Insert Service Call`,
+    'success',
+  );
+};
+const commitListService = (data) => {
+  setAlert(
+    'Reference List Service Call',
+    `Reference List Service Call`,
+    'success',
+  );
+};
+const commitDetailService = (data) => {
+  setAlert(
+    'Commit Detail Service Call',
+    `Commit Detail Service Call`,
+    'success',
+  );
+};
 
 export const services = {
+  ProjectInsertService: projectInsertService,
   ProjectListService: projectListService,
   ProjectDetailService: projectDetailService,
+  ProjectDeleteService: projectDeleteService,
+  ReferenceInsertService: referenceInsertService,
+  ReferenceListService: referenceListService,
+  ReferenceDetailService: referenceDetailService,
+  CommitInsertService: commitInsertService,
+  CommitListService: commitListService,
+  CommitDetailService: commitDetailService,
 };
