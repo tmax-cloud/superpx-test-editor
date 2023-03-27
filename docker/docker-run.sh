be518e4b00ced1d47a3b2c2e14b15f9b9362319c
inth9198 <inth2474@naver.com>
2023-03-27 10:23:59 +0900

    [feat] action, add file 시 파일 생성, readme 파일 마크 다운으로 렌더링

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,4 +1,4 @@
 export WS_URL=ws://172.22.11.2:38080
-export SUPER_PX_VERSION=super-px-0.1.8.1
+export SUPER_PX_VERSION=super-px-0.1.7
 
 docker run -e WS_URL=$WS_URL -e SUPER_PX_VERSION=$SUPER_PX_VERSION -p 8080:8080 superpx-editor:test
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const wsUrl = process.env.WS_URL; //ws://172.22.11.2:38080;
-export const servicePrefix = `${process.env.SUPER_PX_VERSION}/com.tmax.scm.service`; //super-px-0.1.8.1/com.tmax.scm.service;
+export const servicePrefix = `${process.env.SUPER_PX_VERSION}/com.tmax.scm.service`; //super-px-0.1.7/com.tmax.scm.service;
