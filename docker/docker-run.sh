9094f1be8f0f7efa1771770d5048390b9a89a7b2
Park Hyowook <48277126+ccsforever@users.noreply.github.com>
2023-03-30 14:35:51 +0900

    Update docker-run.sh

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,4 +1,4 @@
 export WS_URL=ws://172.22.11.2:38080
 export SUPER_PX_VERSION=super-px-0.1.8.1
 
-docker run -e WS_URL=$WS_URL -e SUPER_PX_VERSION=$SUPER_PX_VERSION -p 80:80 hyperregistry.tmaxcloud.org/ck3-2/superpx-editor:0.0.0
+docker run -e WS_URL=$WS_URL -e SUPER_PX_VERSION=$SUPER_PX_VERSION -p 80:80 $IMAGE_NAME
