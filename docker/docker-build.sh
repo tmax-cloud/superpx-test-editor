46c377a92b1a81aab68392ef9ebe181a47a56675
Hyowook Park <hyowook_park@tmax.co.kr>
2023-04-05 14:36:40 +0900

    [patch][docs] update docker build, Dockerfile

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,1 +1,5 @@
-docker build -t $DOCKER_REGISTRY/$IMAGE_NAME:$TAG .
+export WS_URL=ws://172.22.11.2:38080
+export SUPER_PX_VERSION=super-px-0.1.8.1
+yarn install
+yarn webpack
+docker build -t $DOCKER_REGISTRY/$IMAGE_NAME:$TAG -f ./docker/Dockerfile .
