ff4a30a1a597527c9a667e1e06f7efcbbedab57a
Hyowook Park <hyowook_park@tmax.co.kr>
2023-04-19 16:14:14 +0900

    [refact] useObserver 대신 Observer 사용으로 수정

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,32 +1,37 @@
-import * as React from "react";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
-import alertStore from "../../stores/alertStore";
-import { useObserver } from "mobx-react";
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import alertStore from '../../stores/alertStore';
+import { Observer } from 'mobx-react';
 
 export const BaseAlert: React.FC = () => {
   return (
     <div className="alert">
-      {useObserver(() => (
-        <div>
-          {alertStore.alertList.length > 1 && (
-            <Alert severity="info" onClose={() => alertStore.resetAlertInfo()}>
-              Close All
-            </Alert>
-          )}
-          {alertStore.alertList.map((alert, index) => (
-            <Alert
-              severity={alert.type || "info"}
-              onClose={() => {
-                alertStore.deleteAlertInfo(index);
-              }}
-            >
-              {alert.title && <AlertTitle>{alert.title}</AlertTitle>}
-              {alert.message}
-            </Alert>
-          ))}
-        </div>
-      ))}
+      <Observer>
+        {() => (
+          <div>
+            {alertStore.alertList.length > 1 && (
+              <Alert
+                severity="info"
+                onClose={() => alertStore.resetAlertInfo()}
+              >
+                Close All
+              </Alert>
+            )}
+            {alertStore.alertList.map((alert, index) => (
+              <Alert
+                severity={alert.type || 'info'}
+                onClose={() => {
+                  alertStore.deleteAlertInfo(index);
+                }}
+              >
+                {alert.title && <AlertTitle>{alert.title}</AlertTitle>}
+                {alert.message}
+              </Alert>
+            ))}
+          </div>
+        )}
+      </Observer>
     </div>
   );
 };
